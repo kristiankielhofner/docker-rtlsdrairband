0e5dda300603189cab68e593f09dd090194516fc
Kristian Kielhofner <kris@tovera.com>
2021-08-09 10:45:03 -0500

    Update to use my docker repo, fix sed, don't build non-nfm version

--- buildx.sh
+++ buildx.sh
@@ -3,17 +3,18 @@
 
 set -xe
 
-REPO=fredclausen
+REPO=krisk84
 IMAGE=rtlsdrairband
 PLATFORMS="linux/amd64,linux/arm/v7,linux/arm64"
 
 docker context use default
 export DOCKER_CLI_EXPERIMENTAL="enabled"
-docker buildx use cluster
+#docker buildx use cluster
 
+# Don't built non NFM variant
 # Build & push latest
-docker buildx build --no-cache -t "${REPO}/${IMAGE}:latest" --compress --push --platform "${PLATFORMS}" .
+#docker buildx build --no-cache -t "${REPO}/${IMAGE}:latest" --compress --push --platform "${PLATFORMS}" .
 
-sed "s/NFM_MAKE=\"\"/NFM_MAKE=1/g" < Dockerfile > Dockerfile.NFM
+sed 's/NFM_MAKE=0/NFM_MAKE=1/g' < Dockerfile > Dockerfile.NFM
 
 docker buildx build -f Dockerfile.NFM --no-cache -t "${REPO}/${IMAGE}:latest_nfm" --compress --push --platform "${PLATFORMS}" .
